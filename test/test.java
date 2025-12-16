33ce9a8c4d18fcf3f02d8efe68e725c33bd4e0c3
Dewan Fiam <fiam050811@gmail.com>
2025-12-16 12:28:01 +0600

    Please enter the commit message for your changes.  Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	new file:   test/test.java

---
+++ test/test.java
@@ -0,0 +1,16 @@
+package test;
+
+public class test {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 1;
+        System.out.println(++a);
+        System.out.println(b++);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a++);
+        System.out.println(++b);
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
