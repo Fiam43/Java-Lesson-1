5f64cb67b5218edf821654330bde6944f52f937b
Dewan Fiam <fiam050811@gmail.com>
2025-12-18 11:45:42 +0600

    Please enter the commit message for your changes.  Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   Counter/Main.java
    	new file:   Employee/.vscode/launch.json
    	new file:   Employee/Main.java

---
+++ Counter/Main.java
@@ -0,0 +1,28 @@
+class Counter{
+    int number=10;
+    //static int number=10;
+    void increment(){
+        number=number+1;
+
+    }
+    public static void main(String[] args) {
+        Counter obj1 = new Counter();
+        Counter obj2 = new Counter();
+        Counter obj3 = new Counter();
+        //Guess the Answer
+        obj1.increment();
+        obj2.increment();
+        obj3.increment();
+
+        //To check your answers uncomment the next lines
+
+        //System.out.println(obj1.number);
+        //System.out.println(obj2.number);
+        //System.out.println(obj3.number);
+    }
+}
+
+
+public class Main {
+    
+}
